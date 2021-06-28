89f5eb202057893ead508dab904eeac7c3b4dfa4
Federico Vitale <fedevitale99@gmail.com>
2021-06-28 11:26:51 +0200

    Update aalias.rb

--- Formula/aalias.rb
+++ Formula/aalias.rb
@@ -6,7 +6,7 @@ class Aalias < Formula
   desc "An easy way to add bash aliases"
   homepage "https://github.com/rawnly/aalias"
   url "https://github.com/Rawnly/aalias/releases/latest/download/aalias.tar.gz"
-  sha256 "351acdbe4c3c0cdcdb108399c2517f530da9b99cc2e0219f8fafef0f94ede41f"
+  sha256 "bbb51270be3000b37eed54b4340f8c32f1902562caf2f11769722ab7f084a751"
   version "0.1.1"
 
   def install 
