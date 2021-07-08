9834f67238432ff941d509d4bdf856e4598476bf
Federico Vitale <fedevitale99@gmail.com>
2021-07-09 01:57:06 +0200

    Create splash-rs.rb

---
+++ Formula/splash-rs.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Splash CLI < Formula
+  desc "Download desktop wallpapers with ease"
+  homepage "https://github.com/splash-cli/splash-cli-rs"
+  url "https://github.com/splash-cli/splash-cli-rs/releases/latest/download/splash.tar.gz"
+  sha256 "cba214654a5a214ce2c72dc440554ed7e8990359fd9c5f3b3a4b83ff015f1bb3"
+  version "1.0.3"
+
+  def install 
+    bin.install "splash-rs"
+  end
+end
