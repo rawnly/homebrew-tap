c5c836bb67def01fdfd057e42ab063f031b902f3
Federico <16429579+rawnly@users.noreply.github.com>
2025-03-24 00:41:39 +0100

    fix: update to ai-commit version 0.1.1

--- Formula/ai-commit.rb
+++ Formula/ai-commit.rb
@@ -1,9 +1,9 @@
 class AiCommit < Formula
   desc "A tool to commit with AI"
   homepage "https://github.com/rawnly/ai-commit"
-  url "https://github.com/rawnly/ai-commit/releases/download/0.1.0/ai-commit.tar.gz"
-  sha256 "09ef4a125c476ba7408332e175a8ef32b3409a6ac752e6e574a99a16eb4c42a0"
-  version "0.1.0"
+  url "https://github.com/rawnly/ai-commit/releases/download/0.1.1/ai-commit.tar.gz"
+  sha256 "06958de96e988a9239a1426bea4ee2da87a7f6630eb2c9768b6502a0d89204c5"
+  version "0.1.1"
 
   def install 
     bin.install "ai-commit"
